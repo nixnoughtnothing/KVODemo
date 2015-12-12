d0909aeda68e6e24f62e2db4c8cee13760402698
nixnoughtnothing <ryoh.t778@gmail.com>
2015-12-12 16:29:00 +0900

    add test sample

--- KVODemoTests/KVODemoTests.swift
+++ KVODemoTests/KVODemoTests.swift
@@ -22,6 +22,9 @@ class KVODemoTests: XCTestCase {
     }
     
     func testExample() {
+        
+        XCTAssertNotNil(KvoModel.sharedInstance.myDate, "nil check")
+        
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
